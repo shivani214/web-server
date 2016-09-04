1c97827e8b043c7ebde689ef6deec8363714d1d5
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-04 21:08:42 +0530

    seperated middleware

--- server.js
+++ server.js
@@ -2,26 +2,18 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next){
-        console.log('Private route hit!');
-        next();
-    },
-   logger: function (req, res, next){
-       console.log('Request:' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-       next();
-   }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
 //app.use(middleware.requireAuthentication);
 
 
-app.get('/about',middleware.requireAuthentication, function (req, res) {
-    res.send('About Us!!')
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Us!!');
 });
 app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function () {
     console.log('Express server started in port:' + PORT);
 
